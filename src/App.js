18f8ba744f7dde4e5fb2514dbb2b1c05dbbd2e39
batirovich <batirovich1@gmail.com>
2024-04-05 23:08:25 -0400

    1st project completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import './index.css';
+import BugTracker from "./BugTracker";
+
+function App() {
+  return <div className="App">
+    <BugTracker />
+    {/* <Todo /> */}
+  </div>;
+}
+
+export default App;
